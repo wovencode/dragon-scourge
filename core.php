807c67ca3ef2693db80b2d10400785c08961df4f
Fhiz <support@wovencode.net>
2020-05-20 07:33:49 +0200

    Update core.php
    Everything is now included via the core file

--- core.php
+++ core.php
@@ -1,2 +1,10 @@
 <?php
 
+include_once("config/config.php");
+
+include_once("scripts/cookies.php");
+include_once("scripts/globals.php");
+include_once("scripts/lib.php");
+include_once("scripts/lib2.php");
+include_once("scripts/misc.php");
+include_once("scripts/panels.php");
