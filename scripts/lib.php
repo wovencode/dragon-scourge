94c9a6e12bd0391c36a32743f18cb30203d9a971
Fhiz <support@wovencode.net>
2020-05-20 07:39:55 +0200

    Removed template functions from lib file

--- scripts/lib.php
+++ scripts/lib.php
@@ -29,30 +29,6 @@ $bname = "Consolation Prize Part Deux";
 $bdate = "9.2.2007";
 include("lib2.php");
 
-function gettemplate($templatename) { // SQL query for the template.
-    
-    $filename = "templates/" . $templatename . ".php";
-    include("$filename");
-    return $template;
-    
-}
-
-function parsetemplate($template, $array) { // Replace template with proper content. Also does languages.
-    
-    foreach($array as $a => $b) {
-        $template = str_replace("{{{$a}}}", $b, $template);
-    }
-    return $template;
-    
-}
-
-function getmicrotime() { // Used for timing script operations.
-
-    list($usec, $sec) = explode(" ",microtime()); 
-    return ((float)$usec + (float)$sec); 
-
-}
-
 function is_email($email) { // Thanks to "mail(at)philipp-louis.de" from php.net!
 
     return(preg_match("/^[-_.[:alnum:]]+@((([[:alnum:]]|[[:alnum:]][[:alnum:]-]*[[:alnum:]])\.)+(ad|ae|aero|af|ag|ai|al|am|an|ao|aq|ar|arpa|as|at|au|aw|az|ba|bb|bd|be|bf|bg|bh|bi|biz|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|com|coop|cr|cs|cu|cv|cx|cy|cz|de|dj|dk|dm|do|dz|ec|edu|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gh|gi|gl|gm|gn|gov|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|in|info|int|io|iq|ir|is|it|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|mg|mh|mil|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|museum|mv|mw|mx|my|mz|na|name|nc|ne|net|nf|ng|ni|nl|no|np|nr|nt|nu|nz|om|org|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|pro|ps|pt|pw|py|qa|re|ro|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|sk|sl|sm|sn|so|sr|st|su|sv|sy|sz|tc|td|tf|tg|th|tj|tk|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|um|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)$|(([0-9][0-9]?|[0-1][0-9][0-9]|[2][0-4][0-9]|[2][5][0-5])\.){3}([0-9][0-9]?|[0-1][0-9][0-9]|[2][0-4][0-9]|[2][5][0-5]))$/i",$email));
@@ -99,80 +75,6 @@ function err($error, $system = false, $panels = true) { // Basic little error ha
      $errmsg = "One or more errors have occurred:<br /><br /><b>$error</b><br /><br />Please <a href=\"javascript:history.go(-1);\">go back</a> and try again.";
      display("Error", $errmsg, $panels);
      
-}
-
-function display($title, $content, $panels = true) { // Finalize page and output to browser.
-    
-    include('config.php');
-    global $controlrow, $userrow, $worldrow, $starttime, $version, $build;
-    
-    if (!isset($controlrow)) {
-        $controlrow = dorow(doquery("SELECT * FROM <<control>> WHERE id='1' LIMIT 1"));
-    }
-
-    // Make page tags for XHTML validation.
-    $page = "<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>\n"
-    . "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"DTD/xhtml1-transitional.dtd\">\n"
-    . "<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en\" lang=\"en\">\n";
-    $page .= gettemplate("primary");
-    
-    // Setup for primary page array indexes.
-    $row = array();
-    $row["gamename"] = $controlrow["gamename"];
-    $row["pagetitle"] = $title;
-    $row["background"] = "background" . $userrow["world"];
-    $row["version"] = $version;
-    $row["content"] = $content;
-    $row["moddedby"] = $controlrow["moddedby"];
-    if ($controlrow["forumurl"] != "") { $row["forumslink"] = "<a href=\"".$controlrow["forumurl"]."\">Support Forums</a>"; } else { $row["forumslink"] = ""; }
-    
-    if ($row["moddedby"] != "") {
-        $row["info"] = $row["moddedby"];
-    } else {
-        $row["info"] = "Version <a href=\"index.php?do=version\">" . $row["version"] . "</a> ";
-    }
-    
-    // Setup for side panels.
-    include("panels.php");
-    if ($panels == true) { 
-        $row["leftnav"] = panelleft(); 
-        $row["rightnav"] = panelright();
-        $row["topnav"] = paneltop(true);
-        $row["bottomnav"] = panelbottom();
-        $row["middlenav"] = panelmiddle();
-    } else { 
-        $row["leftnav"] = ""; 
-        $row["rightnav"] = "";
-        $row["topnav"] = paneltop(false);
-        $row["bottomnav"] = "";
-    }
-    
-    $page = rtrim($page, "<-!");
-    
-$page .= <<<THEVERYENDOFYOU
-<table cellspacing="0" cellpadding="3" style="width: 95px; color: #ffffff; border: solid 1px #ffffff; background-color: #000000; margin-top: 2px;">
-  <tr>
-    <td width="40%">
-    {{info}}
-    </td>
-    <td width="20%" style="text-align: center;">
-    {{forumslink}}
-    </td>
-    <td width="40%" style="text-align:right;">
-    <a href="index.php?do=version">Dragon Scourge</a> &copy; by <a href="http://www.renderse7en.com">renderse7en</a>.
-    </td>
-  </tr>
-</table>
-</center></body>
-</html>
-THEVERYENDOFYOU;
-    
-    // Finalize control array for output.
-    $page = parsetemplate($page, $row); 
-    
-    if ($controlrow["compression"] == 1) { ob_start("ob_gzhandler"); }
-    echo $page;
-    die();
 }
 
 ?>
