03c59c2542d7a6fcf4fa1acea27490433139c7c7
Fhiz <support@wovencode.net>
2020-05-21 10:50:45 +0200

    Update globals.php
    Fixed REQUEST_URI errors

--- scripts/globals.php
+++ scripts/globals.php
@@ -14,43 +14,72 @@
 //	within the confines of the Dragon Scourge License Agreement
 //	(see our website for that).
 
-
-#include("core.php");
-
-if (trim(COOKIE_SALT) == "") { die("Invalid setting for secretword in config.php. This setting must never be blank."); }
-
+// ---------------------------------------------------------------------------------------
 // Control row.
+// ---------------------------------------------------------------------------------------
 $controlrow = dorow(doquery("SELECT * FROM <<control>> WHERE id='1' LIMIT 1"));
 
+// ---------------------------------------------------------------------------------------
 // Account row.
-#include("cookies.php");
+// ---------------------------------------------------------------------------------------
 $acctrow = checkcookies();
-if ($acctrow == false && substr($_SERVER["REQUEST_URI"], -21) != "users.php?do=register") { die(header("Location: login.php?do=login")); }
-if ($acctrow != false && $acctrow["characters"] == 0 && substr($_SERVER["REQUEST_URI"], -20) != "users.php?do=charnew") { die(header("Location: users.php?do=charnew")); }
 
+if ($acctrow == false) {
+	$pass = false;
+	
+	if (strpos($_SERVER["REQUEST_URI"], "?do=register") == true || strpos($_SERVER["REQUEST_URI"], "?do=login") == true) {
+		$pass = true;
+	}
+
+	if (!$pass) {
+		die(header("Location: login.php?do=login"));
+	}
+
+}
+
+if ($acctrow != false && $acctrow["characters"] == 0 && strpos($_SERVER["REQUEST_URI"], "?do=charnew") === false) {
+	die(header("Location: users.php?do=charnew"));
+}
+
+// ---------------------------------------------------------------------------------------
 // User row.
-if (substr($_SERVER["REQUEST_URI"], -19) != "login.php?do=logout") {
+// ---------------------------------------------------------------------------------------
+
+if (strpos($_SERVER["REQUEST_URI"], "?do=logout") == true) {
     $online = doquery("UPDATE <<users>> SET onlinetime=NOW() WHERE id='".$acctrow["activechar"]."' LIMIT 1");
 } else {
     $online = doquery("UPDATE <<users>> SET onlinetime = DATE_SUB(onlinetime, INTERVAL 11 MINUTE) WHERE id='".$acctrow["activechar"]."' LIMIT 1");
 }
-$userrow = dorow(doquery("SELECT * FROM <<users>> WHERE id='".$acctrow["activechar"]."' LIMIT 1"));
-if ($userrow != false) { $userrow = array_map("stripslashes", $userrow); }
 
+$userrow = dorow(doquery("SELECT * FROM <<users>> WHERE id='".$acctrow["activechar"]."' LIMIT 1"));
+
+if ($userrow != false)
+{
+	$userrow = array_map("stripslashes", $userrow);
+}
+
+// ---------------------------------------------------------------------------------------
 // World row.
+// ---------------------------------------------------------------------------------------
 $worldrow = dorow(doquery("SELECT * FROM <<worlds>> WHERE id='".$userrow["world"]."' LIMIT 1")); 
 
+// ---------------------------------------------------------------------------------------
 // Town row.
+// ---------------------------------------------------------------------------------------
 if ($userrow["currentaction"] == "In Town") {
     $townrow = dorow(doquery("SELECT * FROM <<towns>> WHERE world='".$userrow["world"]."' AND longitude='".$userrow["longitude"]."' AND latitude='".$userrow["latitude"]."' LIMIT 1"));
 } else {
     $townrow = false;
 }
 
+// ---------------------------------------------------------------------------------------
 // Spells.
+// ---------------------------------------------------------------------------------------
 $spells = dorow(doquery("SELECT * FROM <<spells>> ORDER BY id", "spells"), "id");
 
+// ---------------------------------------------------------------------------------------
 // Global fightrow.
+// ---------------------------------------------------------------------------------------
 $fightrow = array(
     "playerphysdamage"=>0,
     "playermagicdamage"=>0,
