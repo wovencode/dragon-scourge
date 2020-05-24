f8ba898b71e33a504236f957538f661f10726e26
Fhiz <support@wovencode.net>
2020-05-24 18:29:49 +0200

    Update primary.php

--- templates/primary.php
+++ templates/primary.php
@@ -16,46 +16,62 @@
 
 $template = <<<END
 <!DOCTYPE html>
-<head>
-<title>{{gamename}} :: {{pagetitle}}</title>
-<link rel="stylesheet" href="css/primary.css" type="text/css" />
-<script type="text/javascript" src="js/tooltip.js"></script>
-<style type="text/css">
-body { background-image: url(images/{{background}}.jpg); }
-</style>
-</head>
-<body><center>
 
-<table cellspacing="0" cellpadding="3" style="width: 800px; height: 570px; border: solid 1px black; background-color: white; ">
-  <tr>
-    <td colspan="3" style="border-bottom: solid 1px #cccccc;">
-      <table width="100%" cellspacing="0" cellpadding="0"><tr><td width="65%">
-      <a href="index.php"><img src="images/logo.png" alt="{{gamename}}" title="{{gamename}}" /></a>
-      </td><td style="vertical-align:middle; white-space:nowrap;">
-      {{topnav}}
-      </td></tr></table>
-    </td>
-  </tr>
-  <tr>
-    <td valign="top" style="height: 100%; border-right: solid 1px #cccccc;">
-      <img src="images/spacer.gif" alt="" width="140" height="1" /><br />
-      {{leftnav}}
-    </td>
-    <td valign="top" style="height: 100%;">
-      <div style="width: 475px; height: 405px; overflow: auto;">
-      <div class="big"><b>{{pagetitle}}</b></div>
-      {{content}}
-      </div>
-    </td>
-    <td valign="top" style="height: 100%; border-left: solid 1px #cccccc;">
-      <img src="images/spacer.gif" alt="" width="160" height="1" /><br />
-      {{rightnav}}
-    </td>
-  </tr>
-  <tr>
-    <td colspan="3" style="border-top: solid 1px #cccccc;">{{bottomnav}}</td>
-  </tr>
-</table>
+<head>
+	<title>{{gamename}} :: {{pagetitle}}</title>
+	<link rel="stylesheet" href="css/primary.css" type="text/css" />
+	<script type="text/javascript" src="js/tooltip.js"></script>
+	<style type="text/css">
+	body { background-image: url(images/{{background}}.jpg); }
+	</style>
+</head>
+
+<body>
+	<center>
+
+	<table cellspacing="0" cellpadding="3" style="width: 100%; height: 100%; border: solid 1px black; background-color: white; ">
+
+	  <tr>
+		<td colspan="3" style="border-bottom: solid 1px #cccccc;">
+		  <table width="100%" cellspacing="0" cellpadding="0"><tr><td width="65%">
+		  <a href="index.php"><img src="images/logo.png" alt="{{gamename}}" title="{{gamename}}" /></a>
+		  </td><td style="vertical-align:middle; white-space:nowrap;">
+		  {{topnav}}
+		  </td></tr></table>
+		</td>
+	  </tr>
+  
+	  <tr>
+	  
+		<td valign="top" style="height: 100%; border-right: solid 1px #cccccc;">
+		  <img src="images/spacer.gif" alt="" width="140" height="1" /><br />
+		  {{leftnav}}
+		</td>
+		
+		<td valign="top" style="height: 100%;">
+		  <div style="overflow: auto;">
+		  <div class="big"><b>{{pagetitle}}</b></div>
+		  {{content}}
+		  </div>
+		</td>
+		
+		<td valign="top" style="height: 100%; border-left: solid 1px #cccccc;">
+		  <img src="images/spacer.gif" alt="" width="160" height="1" /><br />
+		  {{rightnav}}
+		</td>
+		
+	  </tr>
+  
+	  <tr>
+		<td colspan="3" style="border-top: solid 1px #cccccc;">{{bottomnav}}</td>
+	  </tr>
+  
+	</table>
+
+	</center>
+</body>
+</html>
+
 END;
 
 ?>
